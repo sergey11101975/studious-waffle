30d9a45b522fb0ecccfd1be9f2a04578071fa53e
Sergey11101975 <bogoslovskiisv@yandex.ru>
2023-06-03 16:10:21 +0300

    Возведение в куб

---
+++ lphn/room/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+Console.Write("number=");
+int n = int.Parse(Console.ReadLine());
+
+PrintTableOfCubes(n);
+
+void PrintTableOfCubes(int number){
+    for(int i = 1; i <= number; i++){
+        Console.Write(i * i * i);
+        Console.Write(" ");
+    }
+}
